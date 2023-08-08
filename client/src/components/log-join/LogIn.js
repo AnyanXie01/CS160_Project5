bd242b7cd635731398fc5342e77498b16b512e95
Te-Jung Chen <tchen342@berkeley.edu>
2023-08-08 12:15:56 -0700

    test

--- client/src/components/log-join/LogIn.js
+++ client/src/components/log-join/LogIn.js
@@ -1,3 +1,28 @@
+import logoIcon from "./images/logo.png";
+import "./LogIn.css";
 export function LogIn(){
-    return <div>log in</div>;
+    return (<div className="login-container-outer">
+                <div className="innerContainer">
+                    <div className="logo-container">
+                        <img src={logoIcon} style={{width: '45px', height: '30.452px'}}/>
+                        <h2>INTERNCONNECT</h2>
+                    </div>
+                    <div className="header-container">
+                    <h1>Welcome back</h1>
+                    </div>
+                    <div className="email-input-container">
+                        <div>Email</div>
+                        <input type="email"/>
+                    </div>
+                    <div className="password-input-container">
+                    <div>Email</div>
+                        <input type="email"/>
+                    </div>
+                    <button>Log in</button>
+                    <div>
+                        <div>Don't have an account? </div>
+                        <div>Sign up</div>
+                    </div>
+                </div>
+        </div>);
 }
