1f2af6f35e8d867a9e0c0c55e84cd834a3329f56
Te-Jung Chen <tchen342@berkeley.edu>
2023-08-08 14:55:14 -0700

    test

--- client/src/components/log-join/LogIn.js
+++ client/src/components/log-join/LogIn.js
@@ -1,14 +1,19 @@
 import logoIcon from "./images/logo.png";
 import "./LogIn.css";
+import { Link } from "react-router-dom";
 export function LogIn(){
     return (<div className="login-container-outer">
                 <div className="innerContainer">
-                    <div className="logo-container">
-                        <img src={logoIcon} style={{width: '45px', height: '30.452px'}}/>
-                        <h2>INTERNCONNECT</h2>
+                    <div className="logo-header-container">
+                        <div className="logo-container-login">
+                            <img src={logoIcon} style={{width: '45px', height: '30.452px'}}/>
+                            <h2>INTERNCONNECT</h2>
+                        </div>
+                        <div><h1>Welcome back</h1></div>
                     </div>
-                    <div className="header-container">
-                        <h1>Welcome back</h1>
+                    <div className="username-container">
+                        <div>Username</div>
+                        <input type="text"/>
                     </div>
                     <div className="email-input-container">
                         <div>Email</div>
@@ -19,9 +24,9 @@ export function LogIn(){
                         <input type="password"/>
                     </div>
                     <button>Log in</button>
-                    <div>
+                    <div class="switch-to-sign-up">
                         <div>Don't have an account? </div>
-                        <div>Sign up</div>
+                        <Link to="/join" className="link"><div className="sign-up">Sign up</div></Link>
                     </div>
                 </div>
         </div>);
