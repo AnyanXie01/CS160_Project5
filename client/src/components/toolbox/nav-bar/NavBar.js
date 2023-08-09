0f400c238b1f9e41bfd71b58d2b5190d1ac196a1
Te-Jung Chen <tchen342@berkeley.edu>
2023-08-08 17:16:50 -0700

    test

--- client/src/components/toolbox/nav-bar/NavBar.js
+++ client/src/components/toolbox/nav-bar/NavBar.js
@@ -7,7 +7,7 @@ import logImage from './images/Logo.png';
 import menu from './images/Hamburger.png';
 import { PhoneSideMenu } from "../phone-size-sideMenu/PhoneSideMenu";
 import { useEffect } from "react";
-
+import { useRef } from "react";
 export function NavBar(props){
 
     const [sideMenu, setSideMenu] = useState(false);
@@ -16,7 +16,6 @@ export function NavBar(props){
         setSideMenu(true);
         setShowHamburger(false); 
     }
-
     useEffect(() => {
       const handleResize = () => {
         if (window.innerWidth >= 700) {
@@ -34,9 +33,7 @@ export function NavBar(props){
       return () => {
         window.removeEventListener('resize', handleResize);
       };
-    }, []);
-  
-
+    }, []); 
 
     return(<nav>
                 <div className="nav-container">
@@ -50,7 +47,6 @@ export function NavBar(props){
                         </Link>
                         <Link to="/resume-builder" className="nav-link"><button className={props.navButtonSelection==="resumeBuilder" ? 'selected-nav-Button' : ''}>Resume Builder</button></Link>
                         <Link to="/resources" className="nav-link"><button className={props.navButtonSelection==="resources" ? 'selected-nav-Button' : ''}>Resources</button></Link>
-                        {/* <button><img src={mode} alt="mode switching" className="mode-icon nav-link" style={{ width: "24px", height: "24px"}}/></button> */}
                         <Link to="/profile"><img src={profileImage} alt="profile" className="profile-pic nav-link" style={{ width: "48px", height: "48px"}}/></Link>
                     </div>
                 </div>
@@ -59,7 +55,7 @@ export function NavBar(props){
                     <Link to="/profile"><img src={profileImage} alt="profile" className="profile-pic nav-link" style={{ width: "48px", height: "48px"}}/></Link>
                 </div>
                 
-                { sideMenu ? (<div className="side-menu-container"><PhoneSideMenu/></div>) : (<></>)}
+                { sideMenu ? (<div className="side-menu-container">{sideMenu && <PhoneSideMenu/>}</div>) : (<></>)}
                 
         </nav>);
 }
--- client/src/components/toolbox/phone-size-sideMenu/PhoneSideMenu.js
+++ client/src/components/toolbox/phone-size-sideMenu/PhoneSideMenu.js
@@ -6,8 +6,8 @@ import "./PhoneSideMenu.css";
 
 export function PhoneSideMenu(props){
     return <div className="phone-side-menu">
-            <Link to="/communities" className="nav-link"><button className={props.navButtonSelection==="communities" ? 'selected-nav-Button' : ''}><img src={communitiesIcon} style={{width: "35px", heigth: "35px"}}/>Communities</button></Link>
-            <Link to="/resume-builder" className="nav-link"><button className={props.navButtonSelection==="resumeBuilder" ? 'selected-nav-Button' : ''}><img src={ResumeBuilderIcon} style={{width: "35px", heigth: "35px"}}/>Resume Builder</button></Link>
-            <Link to="/resources" className="nav-link"><button className={props.navButtonSelection==="resources" ? 'selected-nav-Button' : ''}><img src={ResourcesIcon} style={{width: "35px", heigth: "35px"}}/> Resources</button></Link>
+            <Link to="/communities" className="nav-link"><button className={props.navButtonSelection==="communities" ? 'selected-nav-Button' : ''}><img src={communitiesIcon} style={{width: "35px", height: "35px"}}/>Communities</button></Link>
+            <Link to="/resume-builder" className="nav-link"><button className={props.navButtonSelection==="resumeBuilder" ? 'selected-nav-Button' : ''}><img src={ResumeBuilderIcon} style={{width: "35px", height: "35px"}}/>Resume Builder</button></Link>
+            <Link to="/resources" className="nav-link"><button className={props.navButtonSelection==="resources" ? 'selected-nav-Button' : ''}><img src={ResourcesIcon} style={{width: "35px", height: "35px"}}/> Resources</button></Link>
     </div>;
 }
